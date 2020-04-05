0b97c5bb57ec0185a19632ff6276a1b6ec21385f
Andreas Aronsson <andreas.aronsson@jeppesen.com>
2020-04-05 21:00:16 +0200

    Remove unirest and commons-lang3

--- src/main/java/nu/aron/nextbuildnumber/RemoteVersion.java
+++ src/main/java/nu/aron/nextbuildnumber/RemoteVersion.java
@@ -2,15 +2,18 @@ package nu.aron.nextbuildnumber;
 
 import io.vavr.collection.List;
 import io.vavr.control.Option;
-import kong.unirest.Unirest;
+import io.vavr.control.Try;
 import org.apache.maven.artifact.repository.ArtifactRepository;
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.model.Model;
 
+import java.net.URI;
+
 import static java.lang.String.join;
+import static java.net.http.HttpClient.newHttpClient;
+import static java.net.http.HttpRequest.newBuilder;
+import static java.net.http.HttpResponse.BodyHandlers.ofString;
 import static nu.aron.nextbuildnumber.Constants.log;
-import static org.apache.commons.lang3.StringUtils.removeEnd;
-import static org.apache.commons.lang3.StringUtils.substringBetween;
 
 interface RemoteVersion {
 
@@ -26,8 +29,8 @@ interface RemoteVersion {
         return List.ofAll(session.getRequest().getProjectBuildingRequest().getRemoteRepositories())
                 .map(ArtifactRepository::getUrl)
                 .map(u -> urlFromRepo(u, model))
-                .map(u -> Unirest.get(u).asString().getBody())
-                .reject(s -> s.contains("404 Not Found")).toCharSeq().toString();
+                .map(this::responseToString)
+                .reject(this::notFound).toCharSeq().toString();
     }
 
     private Option<String> versionFromString(String data) {
@@ -39,5 +42,25 @@ interface RemoteVersion {
         return join("/", removeEnd(repoUrl, "/"), groupId.replace('.', '/'), model.getArtifactId(), "maven-metadata.xml");
     }
 
+    private String responseToString(String url) {
+        var request = newBuilder(URI.create(url)).build();
+        return Try.of(() -> newHttpClient().send(request, ofString())).get().body();
+    }
 
+    private boolean notFound(String s) {
+        return s.contains("404 Not Found");
+    }
+
+    static String substringBetween(String str, String open, String close) {
+        int start = str.indexOf(open);
+        int end = str.indexOf(close, start + open.length());
+        return str.substring(start + open.length(), end);
+    }
+
+    static String removeEnd(String str, String remove) {
+        if (str.endsWith(remove)) {
+            return str.substring(0, str.length() - remove.length());
+        }
+        return str;
+    }
 }
